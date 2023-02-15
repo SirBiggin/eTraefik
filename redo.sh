756e7cf38da202aceda259bb0efd3542dac882f1
SirBiggin <Eric.G.Greene@hotmail.com>
2023-02-15 09:12:03 -0700

    a

--- redo.sh
+++ redo.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-	apache2-utils \
-	net-tools \
-	git
+cd /etc/
+rm -r /etc/etraefik
 
 cd /etc/
-git clone https://github.com/SirBiggin/eTraefik
+git clone https://github.com/SirBiggin/etraefik
 cd /etc/etraefik/
 sudo mkdir -p /etc/etraefik/certs
 sudo touch /etc/etraefik/certs/acme.json
 sudo chmod 600 /etc/etraefik/certs/acme.json
-cd /etc/traefik/
+cd /etc/etraefik/
 ls - l run.sh
 sudo chmod 774 run.sh
 wget https://github.com/traefik/traefik/releases/download/v2.9.7/traefik_v2.9.7_linux_amd64.tar.gz
 tar -zxvf traefik_v2.9.7_linux_amd64.tar.gz
 cd /etc/etraefik/
 ./run.sh
-
-reboot
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /etc/traefik/
+cd /etc/etraefik/
 ./traefik --configFile=traefik.yml
