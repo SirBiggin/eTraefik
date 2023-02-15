5d8f79b8e171e8554a34e81a3f37b642b3d73b1c
SirBiggin <Eric.G.Greene@hotmail.com>
2023-02-15 09:27:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,12 +13,12 @@ sudo apt-get install \
 	git
 
 cd /etc/
-git clone https://github.com/SirBiggin/eTraefik
+git clone https://github.com/SirBiggin/etraefik
 cd /etc/etraefik/
 sudo mkdir -p /etc/etraefik/certs
 sudo touch /etc/etraefik/certs/acme.json
 sudo chmod 600 /etc/etraefik/certs/acme.json
-cd /etc/traefik/
+cd /etc/etraefik/
 ls - l run.sh
 sudo chmod 774 run.sh
 wget https://github.com/traefik/traefik/releases/download/v2.9.7/traefik_v2.9.7_linux_amd64.tar.gz
